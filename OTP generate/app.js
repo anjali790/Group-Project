75463c04d1bb9ea86593c3cc79e05583d3e02978
anjali790 <anjaliruby790@gmail.com>
2022-09-13 18:17:46 +0530

    signup

---
+++ OTP generate/app.js
@@ -0,0 +1,20 @@
+//otp-generate//
+const otpVerifyBtn = document.querySelector(".verify-OTP-Btn");
+const otpInput = document.querySelector(".otp-input");
+
+otpVerifyBtn.addEventListener("click", () => {
+
+    let obj = {
+        otpInput: otpInput.value,
+    }
+
+    if (otpInput.value === '123456') {
+        let data = JSON.parse(localStorage.getItem('data')) || [];
+        data.push(obj);
+
+        localStorage.setItem('data', JSON.stringify(data));
+        // alert('OTP varified')
+    } else {
+        alert('invalid OTP');
+    }
+});
